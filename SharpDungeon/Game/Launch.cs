37d65ed1b767ae0b76345cc198f8e06616b3db0c
sedyh <joker000@bk.ru>
2017-11-04 20:47:57 +0300

    Started project

---
+++ SharpDungeon/Game/Launch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpDungeon {
+    static class Launch {
+
+        
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Game.Display());
+        }
+    }
+}
